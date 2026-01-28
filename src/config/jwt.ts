923938c1bcde2329160e07d057b1297c9ce06b6f
Tamagotchi9 <voloshko999@gmail.com>
2026-01-28 21:45:27 +0200

    return token alive time to 1h

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('jwt', () => ({
   secret: process.env.JWT_SECRET,
   signOptions: {
-    expiresIn: '1m',
+    expiresIn: '1h',
   },
   refreshTokenExpiresIn: '7d',
 }));
